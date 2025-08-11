dcd4852e1e2ad38f2f3880bfca2346c101e689f9
2459005-dot <2459005@kbu.ac.kr>
2025-08-11 11:58:34 +0900

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,61 +1,26 @@
-import './App.css'
-import { useState, useRef, useEffect } from 'react'
-import { searchWikiTitles } from './api/wiki'
+import './App.scss'
+import { useState } from 'react'
+import Viewer from './components/Viewer'
+import Controller from './components/Controller'
 
 function App() {
-  const [q, setQ] = useState('')
-  const [items, setItems] = useState([])
-  const [loading, setLoading] = useState(false)
-  const [err, setError] = useState('')
-  const inputRef = useRef(null)
 
-  useEffect(() => {
-    inputRef.current.focus()
-    console.log(items)
-  }, [])
+  const [count, setCount] = useState(0)
 
-  const handleSearch = async () => {
-    const keyword = q.trim()
-    if (!keyword) return
-    try {
-      setLoading(true)
-      setError('')
-      const pages = await searchWikiTitles(keyword)
-      setItems(pages)
-      setQ('')
-    } catch (error) {
-      console.log(error)
-      setErr("검색 중 오류 발생")
-      setItems([])
-    } finally {
-      setLoading(false)
-      inputRef.current?.focus()
-    }
+  const onClickButton = (value) => {
+    setCount(count + value)
   }
+  const buttonValues = [-100, -10, -1, 1, 10, 100];
 
   return (
-    <div className='app'>
-      <h1>위키백과 검색</h1>
-      <div className="search">
-        <input ref={inputRef} value={q} onChange={(e) => setQ(e.target.value)}
-          onKeyUp={(e) => e.key === 'Enter' && handleSearch()} type="text" placeholder='검색어를 입력하세요 (예: 서울, 한글, react)' />
-        <button onClick={handleSearch} disabled={loading}>
-          {loading ? "검색중..." : "검색"}
-        </button>
-      </div>
-      {err && <div className="error">에러코드</div>}
-      <ul className='list'>
-        {items.map((p) => (
-
-          <li key={p.id}>
-            <div>
-              <strong>{p.title}</strong>
-              <p>{p.description || '요약 없음'}</p>
-              <a href={`https://ko.wikipedia.org/wiki/${encodeURIComponent(p.title)}`} target='_blank'>위키에서 보기</a>
-            </div>
-          </li>
-        ))}
-      </ul>
+    <div className='App'>
+      <h1>Simple Counter</h1>
+      <section>
+        <Viewer count={count} />
+      </section>
+      <section>
+        <Controller onClickButton={onClickButton}  {...buttonValues} />
+      </section>
     </div>
   )
 }
