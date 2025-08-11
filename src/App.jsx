61af613ca5b1b23d7b8cfcd6b75b75ceca4c9cba
2459005-dot <2459005@kbu.ac.kr>
2025-08-11 12:35:29 +0900

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { Routes, Route, Link } from 'react-router-dom'
 import './App.css'
+import Home from './pages/Home'
+import About from './pages/About'
+import NotFound from './pages/NotFound'
+import Company from './pages/Company'
+import AboutDetail from './pages/AboutDetail'
+import CompanyDetail from './pages/CompanyDetail'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <h1>라우터 실습</h1>
+      <nav>
+        <Link to="/">홈</Link>||
+        <Link to="/about">소개</Link>||
+        <Link to="/company">회사</Link>
+      </nav>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/about/:id' element={<AboutDetail />} />
+        <Route path='/company' element={<Company />} />
+        <Route path='/company/:id' element={<CompanyDetail />} />
+        <Route path='*' element={<NotFound />} />
+      </Routes>
+    </div>
   )
 }
 
